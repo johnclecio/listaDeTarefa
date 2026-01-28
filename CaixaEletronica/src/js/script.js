761f26bfbb9a3253878a0136a7ab8ecc5147f0a2
johnclecio <johncleciofl@gmail.com>
2026-01-28 00:31:32 -0300

    adicionando atividade de javaScript CaixaEletronico

---
+++ CaixaEletronica/src/js/script.js
@@ -0,0 +1,65 @@
+
+
+class ContaBancaria{
+    #saldo
+    constructor(){
+        this.#saldo = 0;
+    }
+
+    depositar(valor){
+        this.#saldo += valor;
+
+    }
+
+    sacar(valor){
+        this.#saldo -= valor;
+    }
+
+    temSaldoParaSacar(valor){
+        return valor <= this.#saldo;
+    }
+
+    get saldo(){
+        return this.#saldo;
+    }
+}
+
+class CaixaEletronica{
+
+    constructor(conta){
+        this.conta = conta;
+
+    }
+    sacar(){
+        const valorSaque = parseFloat(document.getElementById("valorSaque").value);
+
+        if(this.conta.temSaldoParaSacar(valorSaque)){
+            this.conta.sacar(valorSaque);
+            this.mostrarSaldo(this.conta.saldo);
+
+        }else{
+            this.mostrarSaldo("Insuficiente!");
+        }
+    }
+
+    depositar(){
+        const valorDeposito = parseFloat(document.getElementById("valorDepositar").value);
+
+        this.conta.depositar(valorDeposito);
+        this.mostrarSaldo(this.conta.saldo);
+    }
+
+    mostrarSaldo(saldo){
+
+        document.getElementById("saldo").textContent = `Saldo: R$ ${saldo}`;
+        document.getElementById("valorDepositar").value = '';
+        document.getElementById("valorSaque").value = '';
+
+    }
+  
+
+    
+}
+const conta = new ContaBancaria();
+const caixaEletronica = new CaixaEletronica(conta);
+
