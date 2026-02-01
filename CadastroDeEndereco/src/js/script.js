23be6d07a8e0581c6abb3fd21d0bba348271187c
johnclecio <johncleciofl@gmail.com>
2026-02-01 11:39:55 -0300

    integração com API ViaCEP para auto preenchimento de endereço

--- CadastroDeEndereco/src/js/script.js
+++ CadastroDeEndereco/src/js/script.js
@@ -1,3 +1,30 @@
 
 
-document.getElementById("cep")      
+document.getElementById("cep").addEventListener('blur', (event) => {
+    const elemento = event.target;
+    const cepInformado = elemento.value;
+
+    if(!(cepInformado.length === 8)){
+        return
+    }
+
+    fetch(`https://viacep.com.br/ws/${cepInformado}/json/`)
+        .then(response => response.json())
+        .then(data =>{
+            if(!data.error){
+                document.getElementById('logradouro').value = data.logradouro;
+                document.getElementById('bairro').value = data.bairro;
+                document.getElementById('cidade').value = data.localidade;
+                document.getElementById('estado').value = data.uf;
+
+
+            }
+            else{
+                alert("CEP não encontrado");
+            }
+
+        })
+        .catch(error => console.error("Erro ao buscar CEP: ", error));
+        
+        
+})      
