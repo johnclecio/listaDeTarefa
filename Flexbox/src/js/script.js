4ba2dab7d88c0a67602d279ddbb08b1b4d580edf
johnclecio <johncleciofl@gmail.com>
2026-02-21 01:20:38 -0300

    aplicacao da Tarefa M11 teste 1.2

--- Flexbox/src/js/script.js
+++ Flexbox/src/js/script.js
@@ -4,7 +4,33 @@
 function toggleMenu(x) {
     x.classList.toggle("change");
 
-    const menu = document.querySelector(".menu");
+    const menu = document.querySelector(".nav__list");
     menu.classList.toggle("active");
 }
 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const links = document.querySelectorAll(".nav__link");
+
+  links.forEach(function(link){
+
+    link.addEventListener("click", function(e){
+
+      e.preventDefault();
+
+      const secao = this.dataset.section;
+      const destino = document.querySelector("." + secao);
+
+      if(destino){
+        destino.scrollIntoView({
+          behavior: "smooth"
+        });
+      }
+
+    });
+
+  });
+
+});
